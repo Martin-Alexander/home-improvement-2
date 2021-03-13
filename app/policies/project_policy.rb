facc1bdf7c829884a6171a48c653b578c63d8461
Martin-Alexander <martingianna@gmail.com>
2021-03-13 17:52:59 -0500

    added 'admin' field to users table and modified project policy accordingly

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -1,16 +1,18 @@
 class ProjectPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      if user
-        scope.publicly_visable.or(user.projects)
-      else
+      if user.blank?
         scope.publicly_visable
+      elsif user.admin
+        scope.all
+      else
+        scope.publicly_visable.or(user.projects)
       end
     end
   end
 
   def show?
-    record.public? || user_is_owner?
+    record.public? || user_is_owner? || user_is_admin?
   end
 
   def create?
@@ -18,16 +20,20 @@ class ProjectPolicy < ApplicationPolicy
   end
 
   def update?
-    user_is_owner?
+    user_is_owner? || user_is_admin?
   end
 
   def destroy?
-    user_is_owner?
+    user_is_owner? || user_is_admin?
   end
 
   private
 
   def user_is_owner?
     record.user == user
+  end
+
+  def user_is_admin?
+    user.admin
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
     email { "bob-#{rand}@email.com" }
+    admin { false }
 
     after(:build) do |user|
       user.password_confirmation = "123456"
