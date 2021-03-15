f4726f7f3b03bfed9486905dc0be2aab4d2fe932
Martin-Alexander <martingianna@gmail.com>
2021-03-14 20:34:37 -0400

    Fixed issue with turbolinks caching the mobile menu

--- app/javascript/mobile_menu.js
+++ app/javascript/mobile_menu.js
@@ -1,22 +1,26 @@
 export const initializeMobileMenu = () => {
-  const mobileMenu = document.querySelector("#mobile-menu");
-  const pageContent = document.querySelector("#page-content");
-  const openBurgerMenu = document.querySelector("#open-burger-menu");
+  const mobileMenu      = document.querySelector("#mobile-menu");
+  const pageContent     = document.querySelector("#page-content");
+  const openBurgerMenu  = document.querySelector("#open-burger-menu");
   const closeBurgerMenu = document.querySelector("#close-burger-menu");
 
-  openBurgerMenu.addEventListener("click", () => {
-    mobileMenu.classList.add("flex");
-    mobileMenu.classList.remove("hidden");
-
-    pageContent.classList.add("hidden");
-    pageContent.classList.remove("block");
-  });
-
-  closeBurgerMenu.addEventListener("click", () => {
+  const closeMobileMenu = () => {
     mobileMenu.classList.remove("flex");
     mobileMenu.classList.add("hidden");
 
     pageContent.classList.remove("hidden");
     pageContent.classList.add("block");
-  });
+  }
+
+  const openMobileMenu = () => {
+    mobileMenu.classList.add("flex");
+    mobileMenu.classList.remove("hidden");
+
+    pageContent.classList.add("hidden");
+    pageContent.classList.remove("block");
+  }
+
+  openBurgerMenu.addEventListener("click", openMobileMenu);
+  closeBurgerMenu.addEventListener("click", closeMobileMenu);
+  document.addEventListener("turbolinks:before-render", closeMobileMenu);
 }
